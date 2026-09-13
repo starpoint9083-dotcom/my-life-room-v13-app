a9c89219679e696e01ef331bdc5a444e1e0fa788
starpoint9083-dotcom <starpoint9083@gmail.com>
2026-09-14 05:27:32 +0900

    P2 V33: register keyframe asset paths

--- public/master-motion-v33-assets.js
+++ public/master-motion-v33-assets.js
@@ -1,8 +1,17 @@
 (()=>{
 "use strict";
 window.masterMotionV33Frames={
-"master_04_return_home":[],
-"master_05_sit_down":[],
-"master_06_walk_to_window":[]
+  master_04_return_home:[
+    "/assets/motion-v33/return-01.jpg",
+    "/assets/motion-v33/return-02.jpg"
+  ],
+  master_05_sit_down:[
+    "/assets/motion-v33/sit-01.jpg",
+    "/assets/motion-v33/sit-02.jpg"
+  ],
+  master_06_walk_to_window:[
+    "/assets/motion-v33/walk-window-01.jpg",
+    "/assets/motion-v33/walk-window-02.jpg"
+  ]
 };
 })();
